4f3d851d79cdcdb049e28da39fb7fa11d13d7d6d
MIse <494347958@qq.com>
2019-02-26 15:17:25 +0800

    - update options

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,79 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
+const path = require('path')
+const fs = require('fs')
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
+  initializing() {
+    // 打印欢迎语
     this.log(
-      yosay(`Welcome to the cool ${chalk.red('generator-mise-vue-cli')} generator!`)
-    );
-
-    const prompts = [
+      yosay(`Welcome to the shining ${chalk.cyan('generator-mise-vue-cli')} generator!`)
+    )
+  }
+  prompting() {
+    // 让用户选择是否需要包含vuex
+    const prompts = [{
+        type: 'input',
+        name: 'name',
+        message: 'Name of project:',
+        default: path.basename(process.cwd())
+      },
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+        type: 'input',
+        name: 'description',
+        message: 'Description:',
+        default: '',
+      },
+      // {
+      //   type: 'confirm',
+      //   name: 'includeVuex',
+      //   message: 'Would you like to include Vuex in your project?',
+      //   default: false,
+      // }
+    ]
+    return this.prompt(prompts).then(answers => {
+      this.name = answers.name
+      this.description = answers.description
+      // this.includeVuex = answers.includeVuex
+      this.log(chalk.green('name: ', this.name))
+      this.log(chalk.green('description: ', this.description))
+      // this.log(chalk.green('includeVuex: ', this.includeVuex))
+    })
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    // 复制普通文件
+    // https://github.com/sboudrias/mem-fs-editor
+    this.fs.copyTpl(
+      this.templatePath(),
+      this.destinationPath(), {
+        name: this.name
+      }, {}, {
+        globOptions: {
+          dot: true
+        }
+      }
+    )
+    // 根据用户选择，决定是否安装vuex
+    if (this.includeVuex) {
+      const pkgJson = {
+        name: this.name,
+        description: this.description,
+        // dependencies: {
+        //   vuex: '^3.0.1'
+        // }
+      }
+      // Extend or create package.json file in destination path
+      this.fs.extendJSON(this.destinationPath('package.json'), pkgJson)
+    }
   }
 
   install() {
-    this.installDependencies();
+    this.npmInstall()
   }
-};
+
+  end() {
+    this.log(chalk.green('Construction completed!'))
+  }
+}
